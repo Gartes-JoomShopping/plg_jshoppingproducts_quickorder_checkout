fe52f59fdd880f06a0a44ff085811408fd54b212
oleg <sad.net79@gmail.com>
2020-07-29 15:21:31 +0300

    version>0.0.1

--- quickorder_checkout.php
+++ quickorder_checkout.php
@@ -8,5 +8,55 @@ class plgJshoppingCheckoutQuickOrder_checkout extends JPlugin
     {
 
     }
+    function onBeforeSaveNewProductToCart(&$cart, &$temp_product, &$product, &$errors, &$displayErrorMessage){
 
+    }
+
+    function onBeforeSaveUpdateProductToCart(&$cart, &$product, $key, &$errors, &$displayErrorMessage, &$product_in_cart, &$quantity){
+
+    }
+
+    function onAfterAddProductToCart(&$cart, &$product_id, &$quantity, &$attr_id, &$freeattributes, &$errors, &$displayErrorMessage){
+
+    }
+
+    function onAfterRefreshProductInCart(&$quantity, &$cart){
+
+    }
+
+    function onBeforeDisplayCart(&$cart) {
+
+    }
+
+    function onBeforeDisplayCartView(&$view){
+
+    }
+
+    function onBeforeDisplayCheckoutFinish(&$text, &$order_id){
+
+    }
+
+    function onBeforeLoadWishlistRemoveToCart(&$number_id) {
+
+    }
+
+    function onAfterWishlistRemoveToCart(&$cart) {
+
+    }
+
+    function onBeforeDisplayWishlistView(&$view) {
+
+    }
+
+    function onBeforeDisplayMyAccountView(&$view) {
+
+    }
+
+    public function onBeforeDiscountSave(&$coupon, &$cart){
+
+    }
+
+    function onAfterDiscountSave(&$coupon, &$cart){
+
+    }
 }
